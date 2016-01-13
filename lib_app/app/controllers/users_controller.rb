93af0a5b43152c50d97581b8cb38f122174d5909
nathanallen <thenathanator@gmail.com>
2016-01-12 17:47:41 -0800

    user signup form (users#new) and working form submission (users#create)

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
     render :index
   end
 
+  # display the user signup form
+  def new
+    @user = User.new
+    render :new
+  end
+
+  # process signup form data & create new user
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(user_params)
+
+    redirect_to root_path
+  end
+
 end
