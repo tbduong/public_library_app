c57867d41737a52e9c4df9d256e7b9d26317d78b
nathanallen <thenathanator@gmail.com>
2016-01-13 13:45:28 -0800

    > rake db:migrate

--- lib_app/db/schema.rb
+++ lib_app/db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113013701) do
+ActiveRecord::Schema.define(version: 20160113214455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "libraries", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "floor_count"
+    t.integer  "floor_area"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
