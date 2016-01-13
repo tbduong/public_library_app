efe1a8edb5a4e6dbb34d1e14e21e03b74f3da4d7
nathanallen <thenathanator@gmail.com>
2016-01-13 10:14:57 -0800

    flash messages on login/logout

--- lib_app/app/controllers/sessions_controller.rb
+++ lib_app/app/controllers/sessions_controller.rb
@@ -12,8 +12,10 @@ class SessionsController < ApplicationController
     @user = User.confirm(user_params)
     if @user
       login(@user)
+      flash[:notice] = "Successfully logged in."
       redirect_to @user
     else
+      flash[:error] = "Incorrect email or password."
       redirect_to login_path
     end
   end
@@ -21,6 +23,7 @@ class SessionsController < ApplicationController
   # logout current user
   def destroy
     session[:user_id] = nil
+    flash[:notice] = "Successfully logged out."
     redirect_to root_path
   end
 
