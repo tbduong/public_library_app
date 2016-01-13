3359d0c43428be67d0905f817aac450274d9f99d
nathanallen <thenathanator@gmail.com>
2016-01-13 08:12:00 -0800

    login form (sessions#new) and working form submission (sessions#create)

--- lib_app/app/controllers/sessions_controller.rb
+++ lib_app/app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+  # display login form
+  def new
+    @user = User.new
+    render :new
+  end
+
+  # process login form data & create new session
+  def create
+    user_params = params.require(:user).permit(:email, :password)
+    @user = User.confirm(user_params)
+    if @user
+      login(@user)
+      redirect_to @user
+    else
+      redirect_to login_path
+    end
+  end
+
 end
