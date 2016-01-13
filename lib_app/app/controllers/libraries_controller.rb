66083c9779da24bc08646a0f8111f523bed1794e
nathanallen <thenathanator@gmail.com>
2016-01-13 13:51:20 -0800

    libraries#index route, action, view

--- lib_app/app/controllers/libraries_controller.rb
+++ lib_app/app/controllers/libraries_controller.rb
@@ -1,2 +1,9 @@
 class LibrariesController < ApplicationController
+
+  # display list of libraries
+  def index
+    @libraries = Library.all
+    render :index
+  end
+
 end
--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   get "/logout", to: "sessions#destroy"
   post "/sessions", to: "sessions#create"
+
+  get "/libraries", to: "libraries#index"
+
 end
