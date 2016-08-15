cf3c23977ec5082b02a3447058f0dfdc317e1e08
tbduong <tiffaniduong@gmail.com>
2016-08-15 14:21:11 -0700

    method and route to create users online

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
       render :index
   end
 
+  def new
+   @user = User.new
+   render :new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(user_params)
+
+    redirect_to root_path
+  end
+
+  def show
+   @user = User.find_by_id(params[:id])
+   render :show
+  end
+
 end
--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: "users#index"
+
+  get "/users" => "users#index", as: "users"
+  get "/users/new" => "users#new", as: "new_user"
+  post "/users" => "users#create"
+  get "/users/:id" => "users#show", as: "user"
 end
